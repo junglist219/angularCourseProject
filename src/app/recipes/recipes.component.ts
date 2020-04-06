b2abe1deff67fabb01364f771b72a83d07fb2b8b
bsc <Benjamin.Schneider@ti8m.ch>
2020-04-06 10:38:46 +0200

    observables, subscriber & subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {RecipeService} from './recipe.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-recipes',
@@ -10,19 +11,10 @@ import {RecipeService} from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
 
-  selectedRecipe: Recipe;
-
   constructor(
-    private recipeService: RecipeService
   ) { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      );
   }
 
 }
